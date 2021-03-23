09e0e2a4ff713ce609f944d3ca9655f94cbdea6f
Ingar Almklov <ingara@gmail.com>
2021-03-23 16:14:24 +0100

    Update tmux-lazy-session to v1.0.2

--- tmux-lazy-session.rb
+++ tmux-lazy-session.rb
@@ -1,9 +1,9 @@
 class TmuxLazySession < Formula
   desc "Tmux sessions for lazy people"
   homepage "https://github.com/ingara/tmux-lazy-session"
-  url "https://github.com/ingara/tmux-lazy-session/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/ingara/tmux-lazy-session.git", tag: "v1.0.2", revision: "b1475cc0d0f016792714efcaac121a5ca98f1370"
   sha256 "aea455276a1b2f0af26591390a813840d6865657a61cd44b8b16d6e5cc478f08"
-  version "1.0.0"
+  version "v1.0.2"
   license ""
 
   def install
