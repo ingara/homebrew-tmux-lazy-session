092f1cb3b84a1f57d274897a56a8fbe3cdb961d9
Ingar Almklov <ingara@gmail.com>
2021-03-23 14:51:45 +0100

    specify version

--- tmux-lazy-session.rb
+++ tmux-lazy-session.rb
@@ -3,6 +3,7 @@ class TmuxLazySession < Formula
   homepage "https://github.com/ingara/tmux-lazy-session"
   url "https://github.com/ingara/tmux-lazy-session/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "aea455276a1b2f0af26591390a813840d6865657a61cd44b8b16d6e5cc478f08"
+  version "1.0.0"
   license ""
 
   def install
